c54866dcaba91eb8b4ef7d34cf87d97e0b26a50f
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-04-12 17:39:44 -0300

    FIX yt message

--- WhatsDjBotAPI/Utils/MessageTools.cs
+++ WhatsDjBotAPI/Utils/MessageTools.cs
@@ -6,7 +6,7 @@
         {
             string platform = "Desconhecido";
 
-            if(url.Contains("youtube.com")) { platform = "Youtube"; }
+            if(url.Contains("youtube.com") || url.Contains("youtu.be")) { platform = "Youtube"; }
             if(url.Contains("spotify.com")) { platform = "Spotfy"; }
             if(url.Contains("soundcloud.com")) { platform = "SoundClound"; }
             if(url.Contains("deezer.com")) { platform = "Deezer"; }
