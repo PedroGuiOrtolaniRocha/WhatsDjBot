a239b708b6b13591a1849e9d857bc4e62d941b17
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-04-13 21:11:06 -0300

    Tratamento link3

--- WhatsDjBotAPI/Utils/MessageTools.cs
+++ WhatsDjBotAPI/Utils/MessageTools.cs
@@ -2,26 +2,35 @@
 {
     public class MessageTools
     {
-        public static string WhereLinkFrom(string url) 
+        public static string[] PossiblePlatforms = new string[] { "youtu.be", "spotify.com", "youtube.com", "soundcloud.com", "deezer.com" };
+        public static string WhereLinkFrom(string url)
         {
             string platform = "Desconhecido";
 
-            if(url.Contains("youtube.com") || url.Contains("youtu.be")) { platform = "Youtube"; }
-            if(url.Contains("spotify.com")) { platform = "Spotify"; }
-            if(url.Contains("soundcloud.com")) { platform = "Soundcloud"; }
-            if(url.Contains("deezer.com")) { platform = "Deezer"; }
-            
+            if (url.Contains("youtube.com") || url.Contains("youtu.be")) { platform = "Youtube"; }
+            if (url.Contains("spotify.com")) { platform = "Spotify"; }
+            if (url.Contains("soundcloud.com")) { platform = "Soundcloud"; }
+            if (url.Contains("deezer.com")) { platform = "Deezer"; }
+
             return platform;
         }
 
         private static bool ContainsUrl(string message)
         {
-            
-            if (message.Contains("https://"))
+
+            if (message.Contains("https://") || message.Contains("http://") || message.Contains("www."))
             {
                 return true;
             }
-            
+
+            foreach (string platform in PossiblePlatforms)
+            {
+                if (message.Contains(platform))
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
@@ -31,6 +40,33 @@
             if (!ContainsUrl(message)) { return null; }
 
             int startindex = message.IndexOf("https://");
+
+            if (startindex == -1)
+            {
+                startindex = message.IndexOf("http://");
+            }
+
+            if (startindex == -1)
+            {
+                startindex = message.IndexOf("www.");
+
+                if (startindex == -1)
+                {
+                    foreach(string platform in PossiblePlatforms)
+                    {
+                        if (startindex == -1)
+                        {
+                            if (message.Contains(platform))
+                            {
+                                startindex = message.IndexOf(platform);
+                                break;
+                            }
+                        }
+                    }
+                    if (startindex == -1) { return null; }
+                }
+            }
+
             int endindex = message.IndexOf(" ", startindex);
             if (endindex == -1) { endindex = message.Length; }
             
