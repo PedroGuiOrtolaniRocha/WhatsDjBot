94f92ad1978406ae83bdcf716e750a7ccef557c0
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-06 12:00:41 -0300

    Voltei :(

--- WhatsDjBotAPI/Controllers/WhatsResponseController.cs
+++ WhatsDjBotAPI/Controllers/WhatsResponseController.cs
@@ -66,10 +66,9 @@ public class WhatsResponseController : ControllerBase
 
         if(contextMessage.IsGroup && (contextMessage.IsResponse || contextMessage.IsMentioned))
         {
-            string outputMessage
-            await contextMessage.SendResponse();
+            string outputMessage = "Calma vida"
+            await contextMessage.SendResponse(outputMessage);
             Console.WriteLine("Mensagem enviada: " + outputMessage);
-
         }
 
         return Ok();
