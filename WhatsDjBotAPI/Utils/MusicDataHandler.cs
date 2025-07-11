e0bb24602e6ea28dfa303edc0d03cb0650cdfa48
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-11 15:00:23 -0300

    Teste maldito o retorno

--- WhatsDjBotAPI/Utils/MusicDataHandler.cs
+++ WhatsDjBotAPI/Utils/MusicDataHandler.cs
@@ -15,6 +15,8 @@ namespace WhatsDjBotAPI.Utils
         public static async Task<string> GetMusicsByArtistLastFm(string artistName, int qtnd)
         {
 
+            Console.WriteLine($"\n\n\nBuscando músicas do artista: {artistName} com quantidade: {qtnd}\n\n\n");
+
             string apiKey = Environment.GetEnvironmentVariable("LASTFM_APIKEY") ?? throw new Exception("LASTFM_APIKEY environment variable is not set.");
 
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={artistName}&api_key={apiKey}&limit={qtnd}&format=json");
