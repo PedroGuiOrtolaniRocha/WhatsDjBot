799188d93c98c2ecd6c91811a523f104b9468113
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-11 15:12:24 -0300

    Teste maldito o retorno

--- WhatsDjBotAPI/Utils/ChatGenerator.cs
+++ WhatsDjBotAPI/Utils/ChatGenerator.cs
@@ -4,11 +4,34 @@ using System.ClientModel;
 using static WhatsDjBotAPI.Utils.MusicDataHandler.GetMusicsByArtistLastFmArgs;
 using static WhatsDjBotAPI.Utils.MusicDataHandler;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json.Serialization;
 
 namespace WhatsDjBotAPI.Utils
 {
     public class ChatGenerator
     {
+        public class GetMusicsByArtistLastFmArgs
+        {
+            [JsonPropertyName("artistName")]
+            public string? RecipientName { get; set; }
+
+            [JsonPropertyName("qtnd")]
+            public int Message { get; set; }
+        }
+        public static async Task<string> GetMusicsByArtistLastFm(string artistName, int qtnd)
+        {
+
+            Console.WriteLine($"\n\n\nBuscando músicas do artista: {artistName} com quantidade: {qtnd}\n\n\n");
+
+            string apiKey = Environment.GetEnvironmentVariable("LASTFM_APIKEY") ?? throw new Exception("LASTFM_APIKEY environment variable is not set.");
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={artistName}&api_key={apiKey}&limit={qtnd}&format=json");
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            return await response.Content.ReadAsStringAsync();
+        }
         public static async Task<string> GenerateChatResponseAsync(string message, string name)
         {
 
