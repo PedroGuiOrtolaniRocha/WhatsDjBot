afa68a7458bd44b31dc1406c79fe30aceb5a7482
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-11 18:22:57 -0300

    Teste maldito o retorno

--- WhatsDjBotAPI/Utils/ChatGenerator.cs
+++ WhatsDjBotAPI/Utils/ChatGenerator.cs
@@ -34,43 +34,28 @@ namespace WhatsDjBotAPI.Utils
 
             OpenAIClientOptions options  = new OpenAIClientOptions();
             options.Endpoint = new Uri("https://api.groq.com/openai/v1");
-            IChatClient chatClient = new OpenAIClient(new ApiKeyCredential(Environment.GetEnvironmentVariable("GROQ_API_KEY")),options).GetChatClient(Environment.GetEnvironmentVariable("LLM_MODEL")).AsIChatClient();
 
-            List<OpenAI.Chat.ChatTool> chatTools = new List<OpenAI.Chat.ChatTool>
-            {
-                OpenAI.Chat.ChatTool.CreateFunctionTool(
-                    functionName: "GetMusicsByArtistLastFm", // Nome único para a ferramenta
-                    functionDescription: """
-                         A ferramenta é usada para buscar as músicas mais populares ou "top tracks" de um artista específico no Last.fm.
-                         Use esta ferramenta sempre que o usuário perguntar sobre:
-                         - Músicas de um artista (ex: "músicas do Iron Maiden")
-                         - Top músicas / faixas de um artista (ex: "quais as top músicas do Queen")
-                         - Discoteca / repertório de um artista (ex: "me mostre o que o Led Zeppelin já fez")
-                         - Para encontrar faixas ou canções por nome de artista.
-                         O parâmetro 'qtnd' (quantidade) pode ser usado para especificar o número de músicas a serem retornadas,
-                         com um padrão de 5 se não for especificado pelo usuário.
-                         """,
-                    functionParameters: BinaryData.FromString("""
-                         {
-                             "type": "object",
-                             "properties": {
-                                 "artistName": {
-                                     "type": "string",
-                                     "description": "The name of the artist to search for."
-                                 },
-                                 "qtnd": {
-                                     "type": "integer",
-                                     "description": "The number of top tracks to retrieve (e.g., 5, 10). Defaults to 5 if not provided by the user.",
-                                     "default": 5
-                                 }
-                             },
-                             "required": ["artistName"]
-                         }
-                         """),
-                        functionSchemaIsStrict: false
-                    )
-               
+            ChatOptions chatOptions = new ChatOptions 
+            { 
+                Tools = [AIFunctionFactory.Create(async (string artistName, int qtnd) => 
+                {
+                    return await GetMusicsByArtistLastFm(artistName, qtnd);
+                },
+                "GetMusicsByArtistLastFm",
+                """
+                 A ferramenta é usada para buscar as músicas mais populares ou "top tracks" de um artista específico no Last.fm.
+                 Use esta ferramenta sempre que o usuário perguntar sobre:
+                 - Músicas de um artista (ex: "músicas do Iron Maiden")
+                 - Top músicas / faixas de um artista (ex: "quais as top músicas do Queen")
+                 - Discoteca / repertório de um artista (ex: "me mostre o que o Led Zeppelin já fez")
+                 - Para encontrar faixas ou canções por nome de artista.
+                 O parâmetro 'qtnd' (quantidade) pode ser usado para especificar o número de músicas a serem retornadas,
+                 com um padrão de 5 se não for especificado pelo usuário.
+                 """)],
             };
+
+            IChatClient chatClient = new ChatClientBuilder(new OpenAIClient(new ApiKeyCredential(Environment.GetEnvironmentVariable("GROQ_API_KEY")),options).GetChatClient(Environment.GetEnvironmentVariable("LLM_MODEL")).AsIChatClient()).UseFunctionInvocation().Build();
+
 
             List<ChatMessage> chatHistory =
             [
--- WhatsDjBotAPI/Utils/MusicDataHandler.cs
+++ WhatsDjBotAPI/Utils/MusicDataHandler.cs
@@ -4,27 +4,6 @@ namespace WhatsDjBotAPI.Utils
 {
     public class MusicDataHandler
     {
-        public class GetMusicsByArtistLastFmArgs
-        {
-            [JsonPropertyName("artistName")]
-            public string? RecipientName { get; set; }
 
-            [JsonPropertyName("qtnd")]
-            public int Message { get; set; }
-        }
-        public static async Task<string> GetMusicsByArtistLastFm(string artistName, int qtnd)
-        {
-
-            Console.WriteLine($"\n\n\nBuscando músicas do artista: {artistName} com quantidade: {qtnd}\n\n\n");
-
-            string apiKey = Environment.GetEnvironmentVariable("LASTFM_APIKEY") ?? throw new Exception("LASTFM_APIKEY environment variable is not set.");
-
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={artistName}&api_key={apiKey}&limit={qtnd}&format=json");
-            HttpClient client = new HttpClient();
-
-            HttpResponseMessage response = await client.SendAsync(request);
-
-            return await response.Content.ReadAsStringAsync();
-        }
     }
 }
