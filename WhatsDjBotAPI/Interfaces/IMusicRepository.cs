fbe3c9a83edc27b63392f03483089a3aa2b27996
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-21 20:28:10 -0300

    Add memoria

--- WhatsDjBotAPI/Interfaces/IMusicRepository.cs
+++ WhatsDjBotAPI/Interfaces/IMusicRepository.cs
@@ -5,7 +5,7 @@ namespace WhatsDjBotAPI.Interfaces
     public interface IMusicRepository
     {
         public Task<int?> GetRandomMusicIdByPlatform(string platform, string groupId);
-        public Task<int?> GetRandomMusicId(string groupId);
+        public Task<int?> GetRandomMusicId(string? groupId);
         public Task<Music> GetMusicById(int id);
         public Task InsertMusic(Music music, User sender);
     }
