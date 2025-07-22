fbb18f8809bcaf51b92b375cfa1b15f8b9028325
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-07-21 22:09:52 -0300

    DARA BOM

--- WhatsDjBotAPI/Models/Message.cs
+++ WhatsDjBotAPI/Models/Message.cs
@@ -7,7 +7,7 @@ namespace WhatsDjBotAPI.Models
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime DateTime { get; }
         public string texto_bot { get; set; }
         public string texto_user { get; set; }
 
